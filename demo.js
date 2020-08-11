9cac945ae92077ac4300f05e23a60f98d965d470
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-11 18:46:55 -0500

    Update demo.js

--- demo.js
+++ demo.js
@@ -29,7 +29,7 @@ function startConnect() {
     // Connect the client, if successful, call onConnect function
     client.connect({ 
         onSuccess: onConnect,
-        useSSL: true
+        useSSL: true,
     });
 }
 
