9b9a5c648319ed6b9a956f0e94b782749448ebdd
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-11 11:53:45 -0500

    Update demo.js

--- demo.js
+++ demo.js
@@ -28,8 +28,8 @@ function startConnect() {
     //}
    
     const client = mqtt.connect(host, { username, password });
-    const username = "admin";
-    const password = "4887_Alpha";
+    const username = 'admin';
+    const password = '4887_Alpha';
 
     // Set callback handlers
     client.onConnectionLost = onConnectionLost;
