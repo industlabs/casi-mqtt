b72e0914ae99992e622e4427a71d00914cfd872a
root <root@ip-172-31-2-71.us-west-2.compute.internal>
2020-08-11 14:21:39 +0000

    first commit

---
+++ script/config.js
@@ -0,0 +1,20 @@
+url = 'wss://mqtt.casi.io:8084/mqtt';
+// host = '172.16.153.122';	// hostname or IP address
+//host = 'mqtt.casi.io';	// hostname or IP address
+// host = '172.16.153.110';	// hostname or IP address
+//port = 8084;
+//topic = '#';		// topic to subscribe to
+//useTLS = false;
+//username = null;
+//password = null;
+username = "admin";
+password = "4887_Alpha";
+
+// path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
+//    defaults to "/mqtt"
+//    may include query and fragment
+//
+// path = "/mqtt";
+// path = "/data/cloud?device=12345";
+
+//cleansession = true;
